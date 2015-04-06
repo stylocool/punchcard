ee139d306e7403f6e0be86084ed5378fcd54ba20
Jason Pang <jason.pang@gmail.com>
2015-04-07 00:38:47 +0800

    Fixed payment mailer from address

--- app/mailers/payment_mailer.rb
+++ app/mailers/payment_mailer.rb
@@ -1,10 +1,10 @@
 class PaymentMailer < ActionMailer::Base
-  default from: 'sales.punchcard@gmail.com"'
-
+  
   def send_payment(payment)
     @payment = payment
 
     mail(
+        from: 'sales.punchcard@gmail.com',
         to: payment.company.email,
         subject: "Payment for #{payment.months_of_service} months of PunchCard service for #{payment.company.name}"
     )
