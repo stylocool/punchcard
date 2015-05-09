160d5fb6a3f0630daf466704d9095a21283940df
Jason Pang <jason.pang@gmail.com>
2015-05-09 17:17:19 +0800

    Update payroll format

--- app/admin/payrolls/payroll_pdf.rb
+++ app/admin/payrolls/payroll_pdf.rb
@@ -10,8 +10,9 @@ class PayrollPdf < Prawn::Document
 
     company(worker)
     payroll_message(period)
-    worker_table(worker, calculator.days_worked)
+    worker_table(worker, get_payment_date(period), calculator.days_worked)
     payroll_table(calculator.amount, calculator.amount_normal, calculator.amount_overtime, calculator.amount_deduction, view)
+    signature_table
   end
 
   def get_payment_date(period)
@@ -27,7 +28,7 @@ class PayrollPdf < Prawn::Document
 
       company(worker)
       payroll_message(period)
-      worker_table(worker, calculator.days_worked)
+      worker_table(worker, get_payment_date(period), calculator.days_worked)
       payroll_table(calculator.amount, calculator.amount_normal, calculator.amount_overtime, calculator.amount_deduction, view)
 
       index += 1
@@ -47,118 +48,110 @@ class PayrollPdf < Prawn::Document
   end
 
   def payroll_message(period)
-    move_down 30
+    move_down 10
     text 'Payslip for the period of ' + period.strftime('%B %Y'), align: :center, size: 18
-    move_down 30
-    text 'Date: ' + get_payment_date(period)
   end
 
-  def worker_table(worker, days_worked)
-    move_down 30
-    table worker_details(worker, days_worked), width: 720 do
+  def worker_table(worker, payment_date, days_worked)
+    move_down 10
+    table worker_details(worker, payment_date, days_worked), width: 720 do
       cells.border_width = 0
       columns(1).align = :center
       columns(4).align = :center
       self.header = true
-      self.column_widths = {0 => 120, 1 => 40, 2 => 200, 3 => 60, 4 => 40, 5 => 260}
+      self.column_widths = {0 => 60, 1 => 40, 2 => 260, 3 => 120, 4 => 40, 5 => 200}
     end
   end
 
-  def worker_details(worker, days_worked)
+  def worker_details(worker, payment_date, days_worked)
     [
-        ['Work Permit', ':', worker.work_permit, 'Name', ':', worker.name],
-        ['No. of Days Worked', ':', days_worked, 'Trade', ':', worker.trade]
+        ['Date', ':', payment_date, '', '', ''],
+        ['Name', ':', worker.name, 'Work Permit', ':', worker.work_permit],
+        ['Trade', ':', worker.trade, 'No. of Days Worked', ':', days_worked]
     ]
   end
 
   def payroll_table(amount, amount_normal, amount_overtime, amount_deduction, view)
-    move_down 30
-    table payroll_details(amount, amount_normal, amount_overtime, amount_deduction, view), width: 720 do
-      cells[0, 0].borders = [:top, :bottom, :left]
-      cells[0, 1].borders = [:top, :bottom, :right]
-      cells[0, 2].borders = [:top, :bottom, :left]
-      cells[0, 3].borders = [:top, :bottom, :right]
+    move_down 10
+    table payroll_details(amount, amount_normal, amount_overtime, amount_deduction, view), width: 720, cell_style: { inline_format: true } do
+      cells[0, 0].borders = [:top, :bottom, :left, :right]
+      cells[0, 1].borders = [:top, :bottom, :left, :right]
 
-      cells[1, 0].borders = [:left]
-      cells[1, 1].borders = [:right]
-      cells[1, 2].borders = [:left]
-      cells[1, 3].borders = [:right]
+      cells[1, 0].borders = [:top, :bottom, :left, :right]
+      cells[1, 1].borders = [:top, :bottom, :left, :right]
 
-      cells[2, 0].borders = [:left]
-      cells[2, 1].borders = [:right]
-      cells[2, 2].borders = [:left]
-      cells[2, 3].borders = [:right]
+      cells[2, 0].borders = [:top, :bottom, :left, :right]
+      cells[2, 1].borders = [:top, :bottom, :left, :right]
 
-      cells[3, 0].borders = [:top, :bottom, :left]
-      cells[3, 1].borders = [:top, :bottom, :right]
-      cells[3, 2].borders = [:top, :bottom, :left]
-      cells[3, 3].borders = [:top, :bottom, :right]
+      cells[3, 0].borders = [:top, :bottom, :left, :right]
+      cells[3, 1].borders = [:top, :bottom, :left, :right]
 
-      cells[4, 0].borders = [:top, :bottom, :left]
-      cells[4, 1].borders = [:top, :bottom, :right]
-      cells[4, 2].borders = [:top, :bottom, :left]
-      cells[4, 3].borders = [:top, :bottom, :right]
+      cells[4, 0].borders = [:top, :bottom, :left, :right]
+      cells[4, 1].borders = [:top, :bottom, :left, :right]
 
-      cells[5, 0].borders = [:left]
-      cells[5, 1].border_width = 0
-      cells[5, 2].border_width = 0
-      cells[5, 3].borders = [:right]
+      cells[5, 0].borders = [:top, :bottom, :left, :right]
+      cells[5, 1].borders = [:top, :bottom, :left, :right]
 
-      cells[6, 0].borders = [:left]
-      cells[6, 1].border_width = 0
-      cells[6, 2].border_width = 0
-      cells[6, 3].borders = [:right]
+      cells[6, 0].borders = [:top, :bottom, :left, :right]
+      cells[6, 1].borders = [:top, :bottom, :left, :right]
 
-      cells[7, 0].borders = [:left]
-      cells[7, 1].border_width = 0
-      cells[7, 2].border_width = 0
-      cells[7, 3].borders = [:right]
+      cells[7, 0].borders = [:top, :bottom, :left, :right]
+      cells[7, 1].borders = [:top, :bottom, :left, :right]
 
-      cells[8, 0].borders = [:left]
-      cells[8, 1].border_width = 0
-      cells[8, 2].border_width = 0
-      cells[8, 3].borders = [:right]
+      cells[8, 0].borders = [:top, :bottom, :left, :right]
+      cells[8, 1].borders = [:top, :bottom, :left, :right]
 
-      cells[9, 0].borders = [:left]
-      cells[9, 1].border_width = 0
-      cells[9, 2].border_width = 0
-      cells[9, 3].borders = [:right]
+      cells[9, 0].borders = [:top, :bottom, :left, :right]
+      cells[9, 1].borders = [:top, :bottom, :left, :right]
 
-      cells[10, 0].borders = [:left]
-      cells[10, 1].border_width = 0
-      cells[10, 2].border_width = 0
-      cells[10, 3].borders = [:right]
-
-      cells[11, 0].borders = [:bottom, :left]
-      cells[11, 1].border_width = 0
-      cells[11, 2].borders = [:bottom]
-      cells[11, 3].borders = [:right]
-
-      cells[12, 0].borders = [:bottom, :left]
-      cells[12, 1].borders = [:bottom]
-      cells[12, 2].borders = [:bottom]
-      cells[12, 3].borders = [:bottom, :right]
+      cells[10, 0].borders = [:top, :bottom, :left, :right]
+      cells[10, 1].borders = [:top, :bottom, :left, :right]
 
       self.header = true
-      self.column_widths = { 0 => 180, 1 => 180, 2 => 180, 3 => 180 }
+      columns(1).align = :center
+      self.column_widths = { 0 => 360, 1 => 360 }
     end
   end
 
   def payroll_details(amount, amount_normal, amount_overtime, amount_deduction, view)
     [
-      ['Earnings', 'Amount', 'Deductions', 'Amount'], # 0
-      ['Basic Pay', currency(view, amount_normal), 'Deduction', currency(view, amount_deduction)], # 1
-      ['Overtime', currency(view, amount_overtime), '', ''], # 2
-      ['Total Earnings', currency(view, amount_normal + amount_overtime), 'Total Deductions',	currency(view, amount_deduction)], # 3
-      ['Allowance/Claim', '', '', ''], # 4
-      ['', '', '', ''], # 5
-      ['', '', '', ''], # 6
-      ['', '', '', ''], # 7
-      ['', '', '', ''], # 8
-      ['', '', '', ''], # 9
-      ['', '', '', ''], # 10
-      ['', '', '', ''], # 11
-      ['Employer\'s Signature', '', 'Employee\'s Signature', ''] # 12
+      ['<b>Earnings</b>', '<b>Amount</b>'],
+      ['Basic Pay', currency(view, amount_normal)],
+      ['Overtime', currency(view, amount_overtime)],
+      [' ',' '],
+      ['Less: Deduction', currency(view, amount_deduction)],
+      [' ',' '],
+      ['Total Earnings', currency(view, amount_normal + amount_overtime)],
+      [' ',' '],
+      ['Allowance/Claim', ''],
+      [' ',' '],
+      [' ',' ']
+    ]
+  end
+
+  def signature_table
+    move_down 50
+    table signature_details, width: 720 do
+
+      cells[0, 0].borders = [:bottom]
+      cells[0, 1].border_width = 0
+      cells[0, 2].borders = [:bottom]
+      cells[0, 3].border_width = 0
+
+      cells[1, 0].border_width = 0
+      cells[1, 1].border_width = 0
+      cells[1, 2].border_width = 0
+      cells[1, 3].border_width = 0
+
+      self.header = true
+      self.column_widths = {0 => 180, 1 => 180, 2 => 180, 3 => 180}
+    end
+  end
+
+  def signature_details
+    [
+        ['','','',''],
+        ['Employer\'s Signature', '', 'Employee\'s Signature', '']
     ]
   end
 
